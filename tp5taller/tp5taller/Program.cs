1e91a6f02f4f403552d9bf8abc8deac4453df449
LeanProg <leomasterx10@gmail.com>
2021-05-27 17:44:53 -0300

    1

---
+++ tp5taller/tp5taller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tp5taller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
